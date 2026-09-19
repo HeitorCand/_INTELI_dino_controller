005eeddf0eb5242198a3bd6b28c4fc98226b5266
Heitor Candido <heitorfariacandido@sou.inteli.edu.br>
2026-09-19 12:05:49 -0300

    Set pular servo travel to 35 degrees

--- firmware/dino_voice_controller/dino_voice_controller.ino
+++ firmware/dino_voice_controller/dino_voice_controller.ino
@@ -40,7 +40,7 @@
 // "abaixa" foi invertida no hardware, então não precisa mais inverter no
 // software.
 #define SERVO_PULAR_RELEASED_ANGLE 0
-#define SERVO_PULAR_PRESSED_ANGLE 34
+#define SERVO_PULAR_PRESSED_ANGLE 35
 #define SERVO_ABAIXA_RELEASED_ANGLE 0
 #define SERVO_ABAIXA_PRESSED_ANGLE 26
 // Tempo que o braço fica pressionado antes de soltar.
